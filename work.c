cac5891c1f6464b9f682018f03fff26c746537dc
chenxujian616 <13922606186@139.com>
2020-12-26 14:17:45 +0800

    homework code

--- work.c
+++ work.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// 课程名称，用于显示学生选课情况，避免多次调用课程结构体链表
+char courseName[6][20] = {"Chinese", "Math", "English",
+                          "Physics", "Politics", "Chemistry"};
 
 // 课程信息结构体
 typedef struct course
 {
-    // 课程序号
-    int courseNum;
-    char courseName[20];
-    int courseScore;
-    // 已选人数
-    int chooseNum;
-    // 可容纳人数
-    int maxNum;
-    // 链表
+    int courseNum;       /*课程序号*/
+    char courseName[20]; /*课程名称*/
+    int courseScore;     /*课程学分*/
+    int chooseNum;       /*该课程已选人数*/
+    int maxNum;          /*课程最大容纳人数*/
     struct course *next;
 } Course;
 
@@ -22,15 +24,11 @@ Course *courseHead = NULL;
 // 学生信息结构体
 typedef struct student
 {
-    // 学生序号
-    // int studentNum;
-    char studentName[20];
-    // 已选课程编号，数组长度为5
-    int courseNum[5];
-    // 已选课程数量
-    int courseChooseNum;
-    // 已选课程学分
-    int courseScore;
+    char studentName[20]; /*学生姓名*/
+    int courseCode[5];    /*已选课程编号，数组长度为5*/
+    // int courseChooseNum;  /*已选课程数量*/
+    int courseScore; /*已选课程学分*/
+    int courseCount; /*需要选择课程的数量*/
     struct student *next;
 } Student;
 
@@ -52,11 +50,40 @@ void InputCourse(void);
 void ShowCourse(Course *head);
 
 /**
+ * 更新课程结构体链表
+ * @param   head    课程信息结构体链表头
+ * @param   courseCode  课程编号
+ * @return  无
+ * */
+void UpdataCourseList(Course *head, int courseCode);
+
+/**
+ * 选课操作
+ * @param   sp      学生信息结构体指针
+ * @return  无
+ * */
+void ToSelectCourse(Student *sp);
+
+/**
  * 学生选课操作
  * @param   count   学生人数
  * @return  无
  * */
 void StudentSelect(int count);
+
+/**
+ * 显示学生选课情况
+ * @param   head    学生信息结构体链表
+ * @return  无
+ * */
+void ShowStudentCourse(Student *head);
+
+/**
+ * 对学生信息结构体链表进行排序
+ * @param   sp      学生信息结构体链表
+ * @return  无
+ * */
+void SortStudentList(Student *sp);
 
 int main(void)
 {
@@ -64,7 +91,9 @@ int main(void)
     InputCourse();
     int i;
     // 学生总人数
-    int studentCount = rand() % 10 + 1;
+    srand((int)time(NULL));
+    // 测试人数为3人
+    int studentCount = rand() % 3 + 1;
 start:
     printf("\n\t\t欢迎使用学生选课系统!\n");
     printf("菜单：\n");
@@ -92,6 +121,12 @@ start:
         system("cls");
         // 学生选课
         StudentSelect(studentCount);
+        goto start;
+        break;
+    case 3:
+        system("cls");
+        // 显示学生选课情况
+        ShowStudentCourse(studentHead);
         goto start;
         break;
 
@@ -142,11 +177,12 @@ void InputCourse(void)
 void ShowCourse(Course *head)
 {
     Course *p1 = head;
-    printf("课程编号\t课程名称\t学分\t课程已选人数\t课程人数上限\n");
+    printf("课程编号\t课程名称\t学分\t课程剩余人数\t课程人数上限\n");
 
     for (; p1 != NULL; p1 = p1->next)
     {
-        printf("%-16d%-17s%-12d%-16d%d\n", p1->courseNum, p1->courseName, p1->courseScore, p1->chooseNum, p1->maxNum);
+        printf("%-16d%-17s%-12d%-16d%d\n", p1->courseNum, p1->courseName,
+               p1->courseScore, p1->maxNum - p1->chooseNum, p1->maxNum);
     }
 }
 
@@ -154,10 +190,11 @@ void StudentSelect(int count)
 {
     // 创建学生链表
     Student *p1 = NULL, *p2 = NULL;
-    p1 = p2 = (Student *)malloc(sizeof(Student));
 
     for (int i = 0; i < count; i++)
     {
+        printf("有%d名学生需要选课，目前已完成%d人\n", count, i);
+        p1 = (Student *)malloc(sizeof(Student));
         if (i == 0)
         {
             studentHead = p1;
@@ -166,5 +203,84 @@ void StudentSelect(int count)
         {
             p2->next = p1;
         }
+        p2 = p1;
+        // p1->courseChooseNum = 0;
+        p1->courseScore = 0;
+        memset(p1->courseCode, 0, 5 * sizeof(int));
+        // 需要选择3~5门课程，随机数产生
+        srand((int)time(NULL));
+        p1->courseCount = rand() % 3 + 3;
+        // 输入姓名
+        printf("请输入学生名字：");
+        scanf("%s", p1->studentName);
+
+        // 选课操作
+        ToSelectCourse(p1);
     }
+    p2->next = NULL;
+    system("cls");
+    printf("所有同学已完成选课!\n");
+}
+
+void ToSelectCourse(Student *sp)
+{
+    int code = 0;
+
+    for (int i = 0; i < sp->courseCount; i++)
+    {
+        printf("您需要选择%d门课程，目前已选%d门\n", sp->courseCount, i);
+        // 显示当前可选课程
+        ShowCourse(courseHead);
+        printf("请输入课程编号：");
+        scanf("%d", &code);
+
+        // 检查是否有重选情况
+        if (sp->courseCode[0] != code && sp->courseCode[1] != code &&
+            sp->courseCode[2] != code && sp->courseCode[3] != code &&
+            sp->courseCode[4] != code)
+        {
+            UpdataCourseList(courseHead, code);
+
+            sp->courseCode[i] = code;
+            sp->courseScore += code;
+        }
+        else
+        {
+            printf("您已选择该门课程，请重选\n");
+            i--;
+        }
+    }
+    system("cls");
+    printf("%s同学已完成选课！\n", sp->studentName);
+}
+
+void UpdataCourseList(Course *head, int courseCode)
+{
+    Course *p1 = head;
+
+    for (; p1->courseNum != courseCode; p1 = p1->next)
+        ;
+    p1->chooseNum++;
+}
+
+void ShowStudentCourse(Student *head)
+{
+    // SortStudentList(head);
+
+    Student *p1 = head;
+    printf("学生姓名\t已选课程\t学分\n");
+
+    for (; p1 != NULL; p1 = p1->next)
+    {
+        for (int i = 0; i < p1->courseCount; i++)
+        {
+            printf("%-20s%-10s%d\n", p1->studentName,
+                   courseName[p1->courseCode[i] - 1], p1->courseCode[i]);
+        }
+    }
+}
+
+void SortStudentList(Student *sp)
+{
+    return;
 }
