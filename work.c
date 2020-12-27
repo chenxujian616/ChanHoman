7b88e6b194363dbf90e537072e58653e2b3c6e35
chenxujian616 <13922606186@139.com>
2020-12-27 11:15:15 +0800

    homework code

--- work.c
+++ work.c
@@ -54,9 +54,10 @@ void ShowCourse(Course *head);
  * 更新课程结构体链表
  * @param   head    课程信息结构体链表头
  * @param   courseCode  课程编号
- * @return  无
+ * @return  1       课程有学位
+ *          0       课程没有学位
  * */
-void UpdataCourseList(Course *head, int courseCode);
+int UpdataCourseList(Course *head, int courseCode);
 
 /**
  * 选课操作
@@ -75,16 +76,17 @@ void StudentSelect(int count);
 /**
  * 显示学生选课情况
  * @param   head    学生信息结构体链表
+ * @param   control 控制命令
  * @return  无
  * */
-void ShowStudentCourse(Student *head);
+void ShowStudentCourse(Student *head, int control);
 
 /**
  * 交换链表次序
  * 
  * 该交换过程详见https://www.cnblogs.com/bokezhilu/p/7620181.html
- * @param   s1      链表1
- * @param   s2      链表2
+ * @param   s1      学生信息结构体链表1
+ * @param   s2      学生信息结构体链表2
  * @return  无
  * */
 void Swap(Student *s1, Student *s2);
@@ -96,6 +98,20 @@ void Swap(Student *s1, Student *s2);
  * */
 void SortStudentList(Student *sp);
 
+/**
+ * 保存学生选课信息
+ * @param   head    学生信息结构体链表
+ * @return  无
+ * */
+void SaveStudentInfo(Student *head);
+
+/**
+ * 读取学生选课信息
+ * @param   control 控制命令
+ * @return  无
+ * */
+void ReadStudentCourse(int control);
+
 int main(void)
 {
     // 程序运行时从文件读入课程数据
@@ -103,7 +119,7 @@ int main(void)
     int i;
     // 学生总人数
     srand((int)time(NULL));
-    // 测试人数为3人
+    // 测试人数为3人，可以更改测试人数
     int studentCount = rand() % 3 + 1;
 start:
     printf("\n\t\t欢迎使用学生选课系统!\n");
@@ -139,15 +155,20 @@ start:
     case 3:
         system("cls");
         // 显示学生选课情况
-        ShowStudentCourse(studentHead);
+        ShowStudentCourse(studentHead, i);
         goto start;
         break;
     case 4:
         system("cls");
+        // 防止跳过第3步
+        SortStudentList(studentHead);
+        // 保存选课信息
+        SaveStudentInfo(studentHead);
         goto start;
         break;
     case 5:
         system("cls");
+        ReadStudentCourse(i);
         goto start;
         break;
     case 6:
@@ -197,6 +218,7 @@ void InputCourse(void)
     p2->next = NULL;
     free(p1);
     p1 = NULL;
+    fclose(fp);
 }
 
 void ShowCourse(Course *head)
@@ -267,14 +289,20 @@ void ToSelectCourse(Student *sp)
             sp->courseCode[2] != code && sp->courseCode[3] != code &&
             sp->courseCode[4] != code)
         {
-            UpdataCourseList(courseHead, code);
-
-            sp->courseCode[i] = code;
-            sp->courseScore += code;
+            if (UpdataCourseList(courseHead, code) == 1)
+            {
+                sp->courseCode[i] = code;
+                sp->courseScore += code;
+            }
+            else
+            {
+                printf("该门课程已经没有学位，请重选！\n");
+                i--;
+            }
         }
         else
         {
-            printf("您已选择该门课程，请重选\n");
+            printf("您已选择该门课程，请重选！\n");
             i--;
         }
     }
@@ -282,20 +310,40 @@ void ToSelectCourse(Student *sp)
     printf("%s同学已完成选课！\n", sp->studentName);
 }
 
-void UpdataCourseList(Course *head, int courseCode)
+int UpdataCourseList(Course *head, int courseCode)
 {
     Course *p1 = head;
 
-    for (; p1->courseNum != courseCode; p1 = p1->next)
-        ;
-    p1->chooseNum++;
+    for (; p1 != NULL; p1 = p1->next)
+    {
+        if (p1->courseNum == courseCode)
+        {
+            if (p1->chooseNum < p1->maxNum)
+            {
+                p1->chooseNum++;
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
 }
 
-void ShowStudentCourse(Student *head)
+void ShowStudentCourse(Student *head, int control)
 {
-    SortStudentList(head);
+    Student *p1 = NULL;
+    if (control == 3)
+    {
+        SortStudentList(head);
+        p1 = studentHead;
+    }
+    else
+    {
+        p1 = head;
+    }
 
-    Student *p1 = studentHead;
     printf("学生姓名\t\t已选课程\t学分\n");
 
     for (; p1 != NULL; p1 = p1->next)
@@ -313,6 +361,7 @@ void SortStudentList(Student *sp)
     Student *head = sp;
     char control = 1;
 
+    // 按学分从小到大顺序排序
     while (head != NULL)
     {
         Student *headNext = head->next;
@@ -329,6 +378,32 @@ void SortStudentList(Student *sp)
             headNext = headNext->next;
         }
         // 完成第一次交换后，最小值在链表头
+        if (control == 1)
+        {
+            studentHead = head;
+            control = 0;
+        }
+        head = head->next;
+    }
+
+    // 学分相同时，按姓氏首字母从小到大排序
+    head = studentHead;
+    control = 1;
+    while (head != NULL)
+    {
+        Student *headnext = head->next;
+        while (headnext != NULL)
+        {
+            if ((head->courseScore == headnext->courseScore) &&
+                (head->studentName[0] > headnext->studentName[0]))
+            {
+                Swap(head, headnext);
+                Student *tmp = head;
+                head = headnext;
+                headnext = tmp;
+            }
+            headnext = headnext->next;
+        }
         if (control == 1)
         {
             studentHead = head;
@@ -393,4 +468,72 @@ void Swap(Student *s1, Student *s2)
         s2->next = s1Next;
         s2->prev = s1Prev;
     }
+}
+
+void SaveStudentInfo(Student *head)
+{
+    Student *p1 = head;
+    FILE *fp1, *fp2;
+
+    if ((fp1 = fopen("data.txt", "w")) == NULL ||
+        (fp2 = fopen("situation.txt", "w")) == NULL)
+    {
+        printf("找不到相应文件\n");
+        exit(0);
+    }
+
+    fprintf(fp2, "姓名\t课程名称\t学分\n");
+    for (; p1 != NULL; p1 = p1->next)
+    {
+        // 按照姓名，学分，课程编号的顺序存储
+        fprintf(fp1, "%s %d %d %d %d %d %d %d\n", p1->studentName, p1->courseScore,
+                p1->courseCode[0], p1->courseCode[1], p1->courseCode[2],
+                p1->courseCode[3], p1->courseCode[4], p1->courseCount);
+        for (int i = 0; i < p1->courseCount; i++)
+        {
+            fprintf(fp2, "%s\t%s\t%d\n", p1->studentName,
+                    courseName[p1->courseCode[i] - 1], p1->courseCode[i]);
+        }
+        fprintf(fp2, "%s同学总共选择%d门课程，共计%d学分\n", p1->studentName, p1->courseCount, p1->courseScore);
+    }
+    fclose(fp1);
+    fclose(fp2);
+}
+
+void ReadStudentCourse(int control)
+{
+    FILE *fp;
+    if ((fp = fopen("data.txt", "r")) == NULL)
+    {
+        printf("找不到相应文件\n");
+        exit(0);
+    }
+
+    // 创建读取链表
+    Student *head = NULL;
+    Student *p1, *p2;
+    p1 = p2 = (Student *)malloc(sizeof(Student));
+    p1->prev = NULL;
+
+    fscanf(fp, "%s%d%d%d%d%d%d%d", p1->studentName, &p1->courseScore, &p1->courseCode[0],
+           &p1->courseCode[1], &p1->courseCode[2],
+           &p1->courseCode[3], &p1->courseCode[4], &p1->courseCount);
+    head = p1;
+
+    while (!feof(fp))
+    {
+        p2->next = p1;
+        p2 = p1;
+        p1 = (Student *)malloc(sizeof(Student));
+        p1->prev = NULL;
+        fscanf(fp, "%s%d%d%d%d%d%d%d", p1->studentName, &p1->courseScore, &p1->courseCode[0],
+               &p1->courseCode[1], &p1->courseCode[2],
+               &p1->courseCode[3], &p1->courseCode[4], &p1->courseCount);
+    }
+    p2->next = NULL;
+    free(p1);
+    p1 = NULL;
+    fclose(fp);
+
+    ShowStudentCourse(head, control);
 }
